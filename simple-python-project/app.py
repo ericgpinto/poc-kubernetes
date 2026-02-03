1ed9f49f5dc19b776fc156968abc080c15408024
ericgpinto <ericgrandopinto@gmail.com>
2026-02-03 08:05:12 -0300

    feat: working with environment variables

--- simple-python-project/app.py
+++ simple-python-project/app.py
@@ -1,10 +1,13 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Aplication is running!"
+    name = os.getenv('NAME')
+    age = os.getenv('AGE')
+    return f'Hello, my name is {name} and I am {age} years old.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
